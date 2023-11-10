5c0faf887788914c308ee22ec71c96b625909949
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-10 22:04:05 +0530

    basic things added

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,23 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+require("dotenv").config();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ success: true, message: "API is working!" });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
