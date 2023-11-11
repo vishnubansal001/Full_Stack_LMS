a7646a78fecef13e56b364c3f197b645f2f73d7b
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-11 20:42:42 +0530

    error handling done

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ export const app = express();
 require("dotenv").config();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
@@ -21,3 +22,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+app.use(ErrorMiddleware);
--- server/middleware/error.ts
+++ server/middleware/error.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import ErrorHandler from "../utils/ErrorHandler";
+import ErrorHandler from "../utils/ErrorHandler.ts";
 
-module.exports = (
+export const ErrorMiddleware = (
   err: any,
   req: Request,
   res: Response,
