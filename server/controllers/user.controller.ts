1b02dc333d191b927342a4243eb78b8c0d72d01b
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-21 20:20:54 +0530

    added the logout user route

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -161,3 +161,24 @@ export const loginUser = CatchAsyncErrors(
     }
   }
 );
+
+export const logoutUser = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.cookie("access_token", "", {
+        maxAge: 1,
+      });
+
+      res.cookie("refresh_token", "", {
+        maxAge: 1,
+      });
+
+      res.status(200).json({
+        success: true,
+        message: "Logged out successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(400, error.message));
+    }
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -2,12 +2,14 @@ import express from "express";
 import {
   activateUser,
   loginUser,
+  logoutUser,
   registerationUser,
 } from "../controllers/user.controller";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registerationUser);
 userRouter.post("/activate-user", activateUser);
-userRouter.post("/login-user", loginUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", logoutUser);
 
 export default userRouter;
