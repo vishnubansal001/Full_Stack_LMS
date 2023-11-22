f5cc239d0cf8cb3fb9c91c26a47b06c1d8911322
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-22 23:22:56 +0530

    added the validation

---
+++ server/middleware/auth.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { CatchAsyncErrors } from "./catchAsyncErros";
+import ErrorHandler from "../utils/ErrorHandler";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { redis } from "../utils/redis";
+
+export const isAuthenticated = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const access_token = req.cookies.access_token;
+    if (!access_token) {
+      return next(
+        new ErrorHandler(400, "Login first to access this resource.")
+      );
+    }
+    const decoded = jwt.verify(
+      access_token,
+      process.env.ACCESS_TOKEN as string
+    ) as JwtPayload;
+
+    if (!decoded) {
+      return next(
+        new ErrorHandler(400, "access token invalid.")
+      );
+    }
+
+    const user = await redis.get(decoded.id);
+    if (!user) {
+      return next(
+        new ErrorHandler(400, "User not found.")
+      );
+    }
+
+    req.user = JSON.parse(user);
+    next();
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -5,11 +5,12 @@ import {
   logoutUser,
   registerationUser,
 } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registerationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/logout", logoutUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
 
 export default userRouter;
