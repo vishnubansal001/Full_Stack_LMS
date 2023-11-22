90e63d3ce671f906ab9aeaf1a76184fb8ab3809b
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-11-23 00:19:33 +0530

    error fixed

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -201,7 +201,7 @@ export const updateAccessToken = CatchAsyncErrors(
 
       const decoded = jwt.verify(
         refresh_token,
-        process.env.REFRESH_SECRET as string
+        process.env.REFRESH_TOKEN as string
       ) as JwtPayload;
 
       const message = "Could not find user";
